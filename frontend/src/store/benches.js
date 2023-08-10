4374f6cd5b953ec38deaafe7ef2580e024334f66
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-10 13:48:42 -0700

    add addBench action creators

--- frontend/src/store/benches.js
+++ frontend/src/store/benches.js
@@ -1,8 +1,16 @@
 import csrfFetch from "./csrf"
 
-export const SET_BENCHES = 'benches/setBenches';
+// CONSTANTS
 
-export const setBenches = (benches) => ({ type: SET_BENCHES, payload: benches })
+export const SET_BENCHES = 'benches/setBenches';
+export const ADD_BENCH = 'benches/addBench';
+
+// ACTION CREATORS
+
+export const setBenches = benches => ({ type: SET_BENCHES, payload: benches });
+export const addBench = bench => ({ type: ADD_BENCH, payload: bench });
+
+// THUNK ACTION CREATORS
 
 export const fetchBenches = () => async dispatch => {
     const res = await csrfFetch('/api/benches');
@@ -13,10 +21,39 @@ export const fetchBenches = () => async dispatch => {
     }
 }
 
+export const fetchBench = benchId => async dispatch => {
+    const res = await csrfFetch(`/api/benches/${benchId}`);
+
+    if (res.ok) {
+        const bench = await res.json();
+        dispatch(addBench(bench));
+    }
+}
+
+export const createBench = benchData => async dispatch => {
+    const res = await csrfFetch('/api/benches', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(benchData)
+    });
+
+    if (res.ok) {
+        const newBench = await res.json();
+        dispatch(addBench(newBench));
+    }
+}
+
+// REDUCER 
+
 export default function benchesReducer(state = {}, action) {
+    const nextState = { ...state };
+    
     switch (action.type) {
         case SET_BENCHES: 
             return action.payload.benches;
+        case ADD_BENCH: 
+            nextState[action.payload.bench.id] = action.payload.bench;
+            return nextState
         default: 
             return state;
     }
