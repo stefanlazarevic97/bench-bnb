7fd60e2b43452327a237714fa6867afc17f4ebbc
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-10 10:40:33 -0700

    fix naming

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AuthenticateMe
+module BenchBnb
     class Application < Rails::Application
         # Initialize configuration defaults for originally generated Rails version.
         config.load_defaults 7.0
@@ -38,7 +38,7 @@ module AuthenticateMe
 
         config.middleware.use ActionDispatch::Cookies
         config.middleware.use ActionDispatch::Session::CookieStore,
-            key: '_auth_me_session',
+            key: '_bench_bnb_session',
             same_site: :lax,
             secure: Rails.env.production?
     end
